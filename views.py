1c7dcd131c143e49cb82bac6b2459ff3410b246d
shivani tomar <80258536+shivi8827@users.noreply.github.com>
2021-10-19 14:49:05 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,75 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+
+    return render(request, 'index.html') 
+
+#analyzer
+
+def analyze(request):
+    djtext = request.POST.get('text','default')
+   
+
+    
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover','off')
+    extraspaceremover = request.POST.get('extraspaceremover','off')
+   
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};;'"\,<>./?@#$%^&*~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removeed Extra Punc','analyzed_text': analyzed }
+        djtext = analyzed
+        
+    if(fullcaps == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose':'Changed to Uppercase','analyzed_text': analyzed }
+        djtext = analyzed  
+       
+
+    if(newlineremover=="on"):
+        analyzed = ""
+        for char in djtext:
+            if char !="\n" and char!="\r":
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removed Newlines','analyzed_text': analyzed }
+        djtext = analyzed
+       
+
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index,char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
+                
+              
+                analyzed = analyzed + char
+                    
+        params = {'purpose':'Removed Newlines','analyzed_text': analyzed }
+        
+     
+    if(removepunc!="on" and fullcaps!="on" and newlineremover!="on" and  extraspaceremover!="on"):
+        return HttpResponse("Pleas Select Any Operation")
+    return render(request, 'analyze.html', params) 
+
+
+def contact(request):
+
+    return render(request, 'contact.html') 
+
+
+
+ 
+def about(request):
+
+    return render(request, 'about.html')    
